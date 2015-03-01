7d7c6cd25a852deee2c528a13302a0da20017d9c
opeRaptor <testsubjectnamehere@gmail.com>
2015-03-01 03:10:19 +0000

    Initial demo files

---
+++ TX/Enrf24_TXRev1/Debug/Enrf24_TXdemo.d
@@ -0,0 +1,64 @@
+Enrf24_TXdemo.d: ../Enrf24_TXdemo.cpp \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Energia.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/binary.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/avr/dtostrf.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/avr/pgmspace.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/avr/interrupt.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/WCharacter.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/WString.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/HardwareSerial.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Stream.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Print.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Printable.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/new.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/variants/launchpad/pins_energia.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/Enrf24/Enrf24.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Arduino.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Energia.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Print.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/SPI/SPI.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/SPI/utility/spi_430.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/Enrf24/nRF24L01.h \
+ C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/Enrf24/nRF24L01.h
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Energia.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/binary.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/avr/dtostrf.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/avr/pgmspace.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/avr/interrupt.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/WCharacter.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/WString.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/HardwareSerial.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Stream.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Print.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Printable.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/new.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/variants/launchpad/pins_energia.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/Enrf24/Enrf24.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Arduino.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Energia.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/cores/msp430/Print.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/SPI/SPI.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/SPI/utility/spi_430.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/Enrf24/nRF24L01.h:
+
+C:/Program\ Files\ (x86)/energia-0101E0014/hardware/msp430/libraries/Enrf24/nRF24L01.h:
---
+++ TX/Enrf24_TXRev1/Enrf24_TXRev1.cpp
@@ -0,0 +1,77 @@
+#include "Energia.h"
+
+
+void setup();
+void loop();
+void dump_radio_status_to_serialport(uint8_t status);
+
+
+#include <Enrf24.h>
+#include <nRF24L01.h>
+#include <string.h>
+#include <SPI.h>
+
+Enrf24 radio(P2_0, P2_1, P2_2);  // P2.0=CE, P2.1=CSN, P2.2=IRQ
+const uint8_t txaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x01 };
+
+int message[3];
+
+void dump_radio_status_to_serialport(uint8_t);
+
+void setup() {
+  Serial.begin(9600);
+
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
+
+  radio.begin();  // Defaults 1Mbps, channel 0, max TX power
+  dump_radio_status_to_serialport(radio.radioState());
+
+  radio.setTXaddress((void*)txaddr);
+}
+
+void loop() {
+
+	message[0]=1;
+	message[1]=analogRead(A4);
+	message[2]=analogRead(A3);
+
+  Serial.print("Sending packet: ");
+  //Serial.write(message);
+  radio.write(message,sizeof(message));//message,sizeof(message)
+  radio.flush();  // Force transmit (don't wait for any more data)
+  dump_radio_status_to_serialport(radio.radioState());  // Should report IDLE
+  delay(10);
+}
+
+void dump_radio_status_to_serialport(uint8_t status)
+{
+  Serial.print("Enrf24 radio transceiver status: ");
+  switch (status) {
+    case ENRF24_STATE_NOTPRESENT:
+      Serial.println("NO TRANSCEIVER PRESENT");
+      break;
+
+    case ENRF24_STATE_DEEPSLEEP:
+      Serial.println("DEEP SLEEP <1uA power consumption");
+      break;
+
+    case ENRF24_STATE_IDLE:
+      Serial.println("IDLE module powered up w/ oscillators running");
+      break;
+
+    case ENRF24_STATE_PTX:
+      Serial.println("Actively Transmitting");
+      break;
+
+    case ENRF24_STATE_PRX:
+      Serial.println("Receive Mode");
+      break;
+
+    default:
+      Serial.println("UNKNOWN STATUS CODE");
+  }
+}
+
+
